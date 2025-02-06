ab1f98bd254570e540c6a798607d537c8ed78fe2
Aman Kumar <aman100170@gmail.com>
2025-02-06 18:18:52 +0530

    Added toaster

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import { SigninCard } from '@/components/organisms/Auth/SigninCard';
 import { SignupContainer } from '@/components/organisms/Auth/SignupContainer';
+import { Toaster } from '@/components/ui/toaster';
 import { Auth } from '@/pages/Auth/Auth';
 import { Notfound } from '@/pages/Notfound/Notfound';
 
@@ -19,6 +20,8 @@ function App() {
 
         <Route path='/*' element={<Notfound />} />
       </Routes>
+
+      <Toaster />
     </QueryClientProvider>
   );
 }
--- src/hooks/apis/auth/useSignup.js
+++ src/hooks/apis/auth/useSignup.js
@@ -1,15 +1,27 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { signUpRequest } from '@/apis/auth';
+import { useToast } from '@/hooks/use-toast';
 
 export const useSignup = () => {
+    const { toast } = useToast();
+
     const { isPending, isSuccess, error, mutateAsync: signupMutation } = useMutation({
         mutationFn: signUpRequest,
         onSuccess: (data) => {
             console.log('Successfully signed up', data);
+            toast({
+                title: 'Successfully signed up',
+                description: 'You will be redirected to the login page in a few seconds.',
+            });
         },
         onError: (error) => {
             console.error('Failed to sign up', error);
+            toast({
+                title: 'Failed to sign up',
+                description: error.message,
+                variant: 'destructive'
+            });
         }
     });
 
