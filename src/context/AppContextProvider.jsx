4f310708d364ebe30e0b9eac0579482c330dd6cb
Aman Kumar <aman100170@gmail.com>
2025-02-15 18:37:03 +0530

    Added context for messages

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -1,6 +1,7 @@
 import combineContext from '@/utils/combineContext';
 
 import { AuthContextProvider } from './AuthContext';
+import { ChannelMessagesProvider } from './ChannelMessages';
 import { CreateChannelContextProvider } from './CreateChannelContext';
 import { CreateWorkspaceContextProvider } from './CreateWorkspaceContext';
 import { SocketContextProvider } from './SocketContext';
@@ -8,10 +9,11 @@ import { WorkspaceContextProvider } from './WorkspaceContext';
 import { WorkspacePreferencesModalContextProvider } from './WorkspacePreferencesModalContext';
 
 export const AppContextProvider = combineContext(
+    ChannelMessagesProvider,
     AuthContextProvider,
     CreateWorkspaceContextProvider,
     WorkspacePreferencesModalContextProvider,
     CreateChannelContextProvider,
     WorkspaceContextProvider,
-    SocketContextProvider
+    SocketContextProvider,
 );
--- src/hooks/apis/channels/useGetChannelMessages.js
+++ src/hooks/apis/channels/useGetChannelMessages.js
@@ -6,15 +6,16 @@ import { useAuth } from '@/hooks/context/useAuth';
 export const useGetChannelMessages = (channelId) => {
     const { auth } = useAuth();
     
-    const { isFetching, isError, error, data: messages } = useQuery({
-        queryFn: () => getPaginatedMessages({ channelId, limit:10, offset:0, token: auth?.token }),
-        queryKey: ['getPaginatedMessages', channelId]
+    const { isFetching, isError, error, data: messages, isSuccess } = useQuery({
+        queryFn: () => getPaginatedMessages({ channelId, limit:20, offset:0, token: auth?.token }),
+        queryKey: ['getPaginatedMessages']
     });
 
     return {
         isFetching,
         isError,
         error,
-        messages
+        messages,
+        isSuccess
     };
 };
